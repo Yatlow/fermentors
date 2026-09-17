8e9aad2229c7e85703b8a3731bd84b4dddef517d
Yatlow <82391403+Yatlow@users.noreply.github.com>
2026-09-17 11:11:58 +0300

    Mount cooler shipment location alert

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import CoolerUndoControl from './components/cooler/CoolerUndoControl.tsx'
+import CoolerShipmentLocationAlert from './components/cooler/CoolerShipmentLocationAlert.tsx'
 import { startVersionGuard } from './SERVICES/versionGuard'
 import './beerStyles.css'
 
@@ -11,5 +12,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <CoolerUndoControl />
+    <CoolerShipmentLocationAlert />
   </StrictMode>,
 )
