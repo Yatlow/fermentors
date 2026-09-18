9df33b5078d5453d78e6b02271bf2e45dfbb08a0
Yatlow <82391403+Yatlow@users.noreply.github.com>
2026-09-18 14:34:25 +0300

    Switch manual status UI to fast guarded commit

--- src/components/tools/Manualstatusassignment .tsx
+++ src/components/tools/Manualstatusassignment .tsx
@@ -3,7 +3,7 @@ import { useMemo, useState } from "react";
 import type { Fermentor } from "../../App";
 import {
     checkStatusTransition,
-    assignAndRefreshTank,
+    applyManualStatusChange,
     type StatusTransitionCheckResult,
 } from "../../SERVICES/tools/Manualstatus ";
 
@@ -149,11 +149,11 @@ export default function ManualStatusAssignment({ brews, isAdmin }: Props) {
         setErrorMsg("");
 
         try {
-            await assignAndRefreshTank(
+            await applyManualStatusChange(
                 selectedTank.id,
-                checkResult.sheetUrl,
                 targetOption.action,
-                targetOption.tankStatus
+                targetOption.tankStatus,
+                selectedTank.batchNumber
             );
 
             setStep("done");
